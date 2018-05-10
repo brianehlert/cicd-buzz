95127b6288f0ecdac4c2b2df77ccb89ca09338ab
Brian Ehlert <b.ehlert@f5.com>
2018-05-10 15:34:17 -0700

    For the demo

--- buzz/generator.py
+++ buzz/generator.py
@@ -2,8 +2,7 @@ import random
 
 buzz = ('continuous testing', 'continuous integration', 'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
-adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
-    'seriously')
+adverbs = ('remarkably', 'enormously', 'substantially', 'significantly', 'seriously')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
 
 def sample(l, n = 1):
