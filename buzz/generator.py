e0451ca5e41d27ee457e13d299159a8e72959eef
Brian Ehlert <ehlert@olympus.f5net.com>
2018-08-15 00:13:53 -0700

    borrowed

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,6 +1,6 @@
 import random
 
-buzz = ('continuous testing', 'continuous integration', 'continuous deployment', 'continuous improvement', 'devops')
+buzz = ('blue', 'continuous testing', 'continuous integration', 'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly', 'seriously')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
